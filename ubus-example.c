d01ae223be029de4e7b7d6d435c44c655f9adc13
Felix Fietkau <nbd@openwrt.org>
2012-05-19 21:09:50 +0200

    ubus-example: use object notification, add a simple client mode

--- ubus-example.c
+++ ubus-example.c
@@ -16,12 +16,13 @@
 #include "libubus.h"
 
 static struct ubus_context *ctx;
+static struct ubus_watch_object test_event;
 struct blob_buf b;
 
 enum {
 	HELLO_ID,
 	HELLO_MSG,
-	HELLO_LAST
+	__HELLO_MAX
 };
 
 static const struct blobmsg_policy hello_policy[] = {
@@ -33,7 +34,7 @@ static int test_hello(struct ubus_context *ctx, struct ubus_object *obj,
 		      struct ubus_request_data *req, const char *method,
 		      struct blob_attr *msg)
 {
-	struct blob_attr *tb[HELLO_LAST];
+	struct blob_attr *tb[__HELLO_MAX];
 	char *msgstr = "(unknown)";
 	char *strbuf;
 
@@ -44,14 +45,47 @@ static int test_hello(struct ubus_context *ctx, struct ubus_object *obj,
 
 	blob_buf_init(&b, 0);
 	strbuf = blobmsg_alloc_string_buffer(&b, "message", 64 + strlen(obj->name) + strlen(msgstr));
-	sprintf(strbuf, "%s: Hello, world: %s", obj->name, msgstr);
+	sprintf(strbuf, "%s received a message: %s", obj->name, msgstr);
 	blobmsg_add_string_buffer(&b);
 	ubus_send_reply(ctx, req, b.head);
 	return 0;
 }
 
+enum {
+	WATCH_ID,
+	__WATCH_MAX
+};
+
+static const struct blobmsg_policy watch_policy[__WATCH_MAX] = {
+	[WATCH_ID] = { .name = "id", .type = BLOBMSG_TYPE_INT32 },
+};
+
+void test_handle_event(struct ubus_context *ctx, struct ubus_watch_object *w,
+                       uint32_t id)
+{
+	fprintf(stderr, "Object %08x went away\n", id);
+}
+
+static int test_watch(struct ubus_context *ctx, struct ubus_object *obj,
+		      struct ubus_request_data *req, const char *method,
+		      struct blob_attr *msg)
+{
+	struct blob_attr *tb[__WATCH_MAX];
+	int ret;
+
+	blobmsg_parse(watch_policy, __WATCH_MAX, tb, blob_data(msg), blob_len(msg));
+	if (!tb[WATCH_ID])
+		return UBUS_STATUS_INVALID_ARGUMENT;
+
+	test_event.cb = test_handle_event;
+	ret = ubus_watch_object_add(ctx, &test_event, blobmsg_get_u32(tb[WATCH_ID]));
+	fprintf(stderr, "Watching object %08x: %s\n", blobmsg_get_u32(tb[WATCH_ID]), ubus_strerror(ret));
+	return ret;
+}
+
 static const struct ubus_method test_methods[] = {
 	UBUS_METHOD("hello", test_hello, hello_policy),
+	UBUS_METHOD("watch", test_watch, watch_policy),
 };
 
 static struct ubus_object_type test_object_type =
@@ -64,23 +98,62 @@ static struct ubus_object test_object = {
 	.n_methods = ARRAY_SIZE(test_methods),
 };
 
-static struct ubus_object test_object2 = {
-	.name = "test2",
+static struct ubus_object test_client_object = {
 	.type = &test_object_type,
 	.methods = test_methods,
 	.n_methods = ARRAY_SIZE(test_methods),
 };
 
+static void server_main(void)
+{
+	int ret;
+
+	ret = ubus_add_object(ctx, &test_object);
+	if (ret)
+		fprintf(stderr, "Failed to add object: %s\n", ubus_strerror(ret));
+
+	ret = ubus_register_watch_object(ctx, &test_event);
+	if (ret)
+		fprintf(stderr, "Failed to add watch handler: %s\n", ubus_strerror(ret));
+
+	uloop_run();
+}
+
+static void client_main(void)
+{
+	uint32_t id;
+	int ret;
+
+	ret = ubus_add_object(ctx, &test_client_object);
+	if (ret) {
+		fprintf(stderr, "Failed to add_object object: %s\n", ubus_strerror(ret));
+		return;
+	}
+
+	if (ubus_lookup_id(ctx, test_object.name, &id)) {
+		fprintf(stderr, "Failed to look up test object\n");
+		return;
+	}
+
+	blob_buf_init(&b, 0);
+	blobmsg_add_u32(&b, "id", test_client_object.id);
+	ubus_invoke(ctx, id, "watch", b.head, NULL, 0, 3000);
+	uloop_run();
+}
+
 int main(int argc, char **argv)
 {
 	const char *ubus_socket = NULL;
-	int ret = 0;
+	bool client = false;
 	int ch;
 
-	while ((ch = getopt(argc, argv, "s:")) != -1) {
+	while ((ch = getopt(argc, argv, "cs:")) != -1) {
 		switch (ch) {
 		case 's':
 			ubus_socket = optarg;
+			break;
+		case 'c':
+			client = true;
 			break;
 		default:
 			break;
@@ -90,25 +163,23 @@ int main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
+	uloop_init();
+
 	ctx = ubus_connect(ubus_socket);
 	if (!ctx) {
 		fprintf(stderr, "Failed to connect to ubus\n");
 		return -1;
 	}
 
-	ret = ubus_add_object(ctx, &test_object);
-	if (ret)
-		fprintf(stderr, "Failed to add_object object: %s\n", ubus_strerror(ret));
-
-	ret = ubus_add_object(ctx, &test_object2);
-	if (ret)
-		fprintf(stderr, "Failed to add_object object: %s\n", ubus_strerror(ret));
-
-	uloop_init();
 	ubus_add_uloop(ctx);
-	uloop_run();
-	uloop_done();
+
+	if (client)
+		client_main();
+	else
+		server_main();
 
 	ubus_free(ctx);
+	uloop_done();
+
 	return 0;
 }
