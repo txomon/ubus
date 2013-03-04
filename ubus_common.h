29a2ed2f17350f29680fd7cdcae9d3a1e3e5886b
Thomas Gstädtner <thomas@gstaedtner.net>
2013-03-04 19:05:08 +0100

    make UBUS_UNIX_SOCKET configurable via cmake
    this is mainly useful to have a single location to read the setting
    from, when it is used multiple times at build time (which it will in a
    following patch) :)

--- ubus_common.h
+++ ubus_common.h
@@ -14,8 +14,6 @@
 #ifndef __UBUS_COMMON_H
 #define __UBUS_COMMON_H
 
-#define UBUS_UNIX_SOCKET "/var/run/ubus.sock"
-
 #define UBUS_SIGNATURE_METHOD	(BLOBMSG_TYPE_LAST + 1)
 #define UBUS_SIGNATURE_END		(BLOBMSG_TYPE_LAST + 2)
 
