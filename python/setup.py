14c8da009430cd084dfa794d86d8b6cc27c85cc7
Javier Domingo Cansino <javier.domingo@fon.com>
2013-11-26 14:15:18 +0100

    python: Adding initial setup, not tested

---
+++ python/setup.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+
+import re
+
+from setuptools import setup, find_packages
+
+try:
+    import ubus
+except ImportError:
+    # We're installing, so cffi isn't available yet
+    ext_modules = []
+else:
+    # We're building bdist, so cffi is available
+    ext_modules = [ubus.ffi.verifier.get_extension()]
+
+
+def get_version(filename):
+    init_py = open(filename).read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+    return metadata['version']
+
+
+setup(
+    name='pyubus',
+    version=get_version('ubus/__init__.py'),
+    url='http://github.com/txomon/ubus',
+    license='Apache License, Version 2.0',
+    author='Javier Domingo Cansino',
+    author_email='javierdo1@gmail.com',
+    description='Python wrapper for libubus',
+    long_description=open('README.rst').read(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    include_package_data=True,
+    ext_package='ubus',
+    ext_modules=ext_modules,
+    install_requires=[
+        'cffi >= 0.6',
+    ]
+)
---
+++ python/ubus/__init__.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+
+import logging
+import os
+import weakref
+
+import cffi
+
+
+__version__ = '0.0.0'
+
+
+# Log to nowhere by default. For details, see:
+# http://docs.python.org/2/howto/logging.html#library-config
+logging.getLogger('ubus').addHandler(logging.NullHandler())
+
+
+_header_file = os.path.join(os.path.dirname(__file__), 'ubus.processed.h')
+_header = open(_header_file).read()
+#_header += '#define SPOTIFY_API_VERSION ...\n'
+ffi = cffi.FFI()
+ffi.cdef(_header)
+lib = ffi.verify("""
+    #include <libubus.h>
+    #include <libubox/blobmsg.h>
+    #include <libubox/blobmsg_json.h>""",
+    libraries=[str('ubus'),
+               str('ubox')],
+    ext_package='ubus')
+
+
+# Mapping between keys and objects that should be kept alive as long as the key
+# is alive. May be used to keep objects alive when there isn't a more
+# convenient place to keep a reference to it. The keys are weakrefs, so entries
+# disappear from the dict when the key is garbage collected, potentially
+# causing objects associated to the key to be garbage collected as well. For
+# further details, refer to the CFFI docs.
+weak_key_dict = weakref.WeakKeyDictionary()
+
+# Mapping between simple keys, like ints and bytestrings that can be passed as
+# userdata through C context, and Python objects used to call the corresponding
+# callback functions in Python context.
+callback_dict = {}
+
+# Reference to the spotify.Session instance. Used to enforce that one and only
+# one session exists in each process.
+#session_instance = None
+
+
+#from spotify.album import *  # noqa
