d72a6dcd2e06d2ecf30688f8d632d5b922892345
Javier Domingo Cansino <javier.domingo@fon.com>
2013-11-28 14:26:41 +0100

    python: Adding gc_collect and tmpdir cleanup to tests

---
+++ python/tests/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+import cffi
+
+
+cffi.verifier.cleanup_tmpdir()
+
+def gc_collect():
+    """Run enough GC collections to make object finalizers run."""
+    if platform.python_implementation() == 'PyPy':
+        # Since PyPy use garbage collection instead of reference counting
+        # objects are not finalized before the next major GC collection.
+        # Currently, the best way we have to ensure a major GC collection has
+        # run is to call gc.collect() a number of times.
+        [gc.collect() for _ in range(10)]
+    else:
+        gc.collect()
